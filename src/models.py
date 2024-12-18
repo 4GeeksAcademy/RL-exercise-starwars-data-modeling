e9cc473dfdf86b9d65fbadddec483845bc1b90f8
Rodrigo Lau Ramos <darkbass16@gmail.com>
2024-12-18 14:17:20 +0000

    start

--- src/models.py
+++ src/models.py
@@ -7,23 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    subdate = Column(String(250), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'Characters'
+    
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    Name = Column(String(250), nullable=False)
+    Appearances = Column(String(250))
+    Affiliations = Column(String(250))
+    Location = Column(String(250))
+    Gender = Column(String(250))
+    Species = Column(String(250))
+    user = relationship(User)
+
+class Locations(Base):
+    __tablename__ = 'Locations'
+    
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    Name = Column(String(250), ForeignKey('Characters.Location'))
+    Planet = Column(String(250))
+    Terrain = Column(String(250))
+    Species = Column(String(250))
+    Resource = Column(String(250))
+    Affiliations = Column(String(250))
+    user = relationship(User)
+
+class Movies(Base):
+    __tablename__ = 'Movies'
+    
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    Name = Column(String(250), ForeignKey('Characters.Appearances'))
+    Runtime = Column(String(250))
+    Rating = Column(String(250))
+    ReleaseDate = Column(String(250))
+    Genre = Column(String(250))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
